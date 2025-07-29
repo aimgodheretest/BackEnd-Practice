ba601a91ec824db980a08039472e1cba7b53f960
Murli Kumar <aimgodhere@gmail.com>
2025-07-29 11:25:24 +0530

    toke updated for user verification

--- aunthetication/src/routes/auth.routes.js
+++ aunthetication/src/routes/auth.routes.js
@@ -52,4 +52,33 @@ router.post("/login", async (req, res) => {
   });
 });
 
+router.get("/user", async (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel
+      .findOne({
+        _id: decoded.id,
+      })
+      .select("-password -__v");
+
+    res.status(401).json({
+      message: "User Data Fetched Successfully",
+      user,
+    });
+
+    res.send(decoded);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized - Invalid User",
+    });
+  }
+});
+
 module.exports = router;
